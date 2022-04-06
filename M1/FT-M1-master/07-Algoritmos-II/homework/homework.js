1a8b859e3eb1682fd5d5c40474f7ddf1c5182f72
Santiago Rivanegra <santiagorivanegra@hotmail.com>
2022-04-06 15:55:42 -0300

    Finish HW A2

--- M1/FT-M1-master/07-Algoritmos-II/homework/homework.js
+++ M1/FT-M1-master/07-Algoritmos-II/homework/homework.js
@@ -6,7 +6,20 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  var flag = true
+  while(flag){
+    var flag = false
+    for(let i = 0; i < array.length-1; i++){
+      let j = i+1
+      if(array[j] < array[i]){
+        let aux = array[i]
+        array[i] = array[j]
+        array[j] = aux
+        var flag = true
+      }
+    }
+  }   
+  return array
 }
 
 function mergeSort(array) {
@@ -14,7 +27,28 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+    if(array.length === 1) return array
+    var half = Math.floor(array.length/2)
+    var left = array.slice(0, half)
+    var right = array.slice(half)
+  return merge(mergeSort(left), mergeSort(right))
+}
 
+function merge(left, right) {
+  var izq = 0
+  var der = 0
+  var mergeArray = []
+
+  while(izq < left.length && der < right.length){
+    if(left[izq] < right[der]){
+      mergeArray.push(left[izq])
+    izq++
+    } else {
+      mergeArray.push(right[der])
+      der++
+    }   
+  }
+  return mergeArray.concat(left.slice(izq)).concat(right.slice(der))
 }
 
 // No modificar nada debajo de esta línea
