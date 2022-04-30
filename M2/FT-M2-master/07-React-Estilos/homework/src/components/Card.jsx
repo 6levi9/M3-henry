7e2a5f5268016d5d10d773f20817f167258c4b90
Santiago Rivanegra <santiagorivanegra@hotmail.com>
2022-04-30 19:14:48 -0300

    Teoria retirada

--- M2/FT-M2-master/07-React-Estilos/homework/src/components/Card.jsx
+++ M2/FT-M2-master/07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import s from './card.module.css';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+    <div className={s.contentCard}>
+      <button onClick={props.onClose} className={s.button}>X</button>
+      <h4>{props.name}</h4>
+    <div className={s.infoCard}>  
+       <div>
+        <p>Min</p>
+        <p>{props.min}</p>
+      </div>
+      <div>  
+       <p>Max</p>
+        <p>{props.max}</p>
+      </div>  
+      <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt ="image not found"/>
+    </div>
+    </div>)
 };
--- M2/FT-M2-master/07-React-Estilos/homework/src/components/SearchBar.jsx
+++ M2/FT-M2-master/07-React-Estilos/homework/src/components/SearchBar.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import s from  './search.module.css'
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return (
+    <div className={s.bar}>
+      <input type="text" placeholder="City..."/>
+      <button onClick={() => props.onSearch("Buscando...")} className={s.button}>Search</button>
+    </div>)
 };
